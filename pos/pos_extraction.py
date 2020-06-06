93aa1151e3ec2b1d26657300974eff3d93491e24
AudayBerro <audayberro@gmail.com>
2020-06-06 14:37:23 +0200

    add expand_contraction and sentence_pos function

--- pos/pos_extraction.py
+++ pos/pos_extraction.py
@@ -1,9 +1,11 @@
 import spacy
+import contractions
+from spacy.lang.en.stop_words import STOP_WORDS
 
 """ Part of Speech extraction using Spacy library """
 
 
-def postExtraction(file_name):
+def pos_extraction(file_name):
   """
   Part of Speech extraction
   :param file_name: file name to access data on which to apply POS extraction
@@ -33,3 +35,72 @@ def postExtraction(file_name):
     response[line.rstrip('\n')] = value
   
   return response
+
+def pos_extraction2(file_name,tags):
+  """
+  Specific Part of Speech extraction to generate new data corpus (apply Weak supervision approach)
+  :param file_name: file name to access data on which to apply POS extraction
+  :param tags: list of tag to extract for each sentence
+  :return a list containing the new generated data
+  """
+  
+  nlp = spacy.load('en_core_web_lg') # en_core_web_lg
+
+  f=open(file_name, "r")
+  response = []
+  while True:
+    # Get next line from file 
+    line = f.readline()
+    if not line: 
+      break
+
+    sentences = nlp(line)
+    para = [] 
+    for token in sentences:
+      if token.text == '\n':
+        continue
+      if token.pos_ in tags:
+        para.append(token.text)
+    value = " ".join(para)
+    response.append(value)
+  
+  return response
+
+def expand_contractions(text):
+    """ expand shortened words, e.g. don't to do not """
+
+    #pycontraction library
+    # Choose model accordingly for contractions function
+    # model = api.load("glove-twitter-25")
+    # # model = api.load("glove-twitter-100")
+    # # model = api.load("word2vec-google-news-300")cont = Contractions(kv_model=model)
+    # cont.load_models()
+    # text = list(cont.expand_texts([text], precise=True))[0]
+
+    result = contractions.fix(text)
+    return result
+
+def sentence_pos(sentence,tags):
+  """
+  Specific Part of Speech extraction to generate new data corpus (apply Weak supervision approach)
+  :param sentence: sentence
+  :param tags: list of tag to extract for each sentence
+  :return a list containg word that have the same POS tags defined by tags and the sentence in a form of list after tokenization
+  """
+  
+  nlp = spacy.load('en_core_web_lg') # en_core_web_lg
+
+  # sentence = expand_contractions(sentence) #expand contraction e.g can't -> can not
+
+  sentences = nlp(sentence)
+  candidate = []
+  tokenized_sentence = []
+  
+  for token in sentences:
+    if token.text == '\n':
+      continue
+    tokenized_sentence.append(token.text) #add token
+    if token.pos_ in tags and token.text not in STOP_WORDS:
+      candidate.append(token.text)
+  
+  return candidate,tokenized_sentence
