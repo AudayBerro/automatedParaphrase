f936e57119accaaf3a4a3a12c39ffcab041fc3d2
AudayBerro <audayberro@gmail.com>
2020-06-09 23:33:15 +0200

    add tr parameter and remove api_key param in translate function

--- translator/yandex_translator.py
+++ translator/yandex_translator.py
@@ -19,18 +19,16 @@ def replace_quote(utterance):
     
     return utterance
 
-def translate(utterance,source,target,api_key):
+def translate(utterance,source,target,tr):
     """
     Translate a sentence
     :param utterance: sentence to translate
     :param source: source language
     :param target: target language
-    :param api_key: Yandex Translate API token https://translate.yandex.com/developers/keys
+    :param tr: yandex Translator object
     :return Translated utterance 
     """
 
-    tr = yandex_translate.Translater()
-    tr.set_key(api_key) 
     tr.set_from_lang(source)
     tr.set_to_lang(target)
     tr.set_text(utterance) #text_to_translate
@@ -45,103 +43,107 @@ def multi_translate(utterance,api_key,pivot_level):
   :param pivot_level: integer that indicate the pivot language level, single-pivot or multi-pivot range,1 =single-pivot, 2=double-pivot, 0=apply single and double
   :return list of utterance translations
   """
+
+  tr = yandex_translate.Translater() # load yandex translator
+  tr.set_key(api_key) # set the api token
+
   response = set()
   text = utterance
   if pivot_level == 0 or pivot_level == 1:
-    tmp = translate(text,'en','it',api_key)
+    tmp = translate(text,'en','it',tr)
     response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'it','en',api_key)
+    tmp = translate(tmp,'it','en',tr)
     response.add(tmp)
 
-    tmp = translate(text,'en','ru',api_key)
+    tmp = translate(text,'en','ru',tr)
     response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'ru','en',api_key)
+    tmp = translate(tmp,'ru','en',tr)
     response.add(tmp)
 
-    tmp = translate(text,'en','ar',api_key)
+    tmp = translate(text,'en','ar',tr)
     response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'ar','en',api_key)
+    tmp = translate(tmp,'ar','en',tr)
     response.add(tmp)
 
-    tmp = translate(text,'en','fr',api_key)
+    tmp = translate(text,'en','fr',tr)
     response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'fr','en',api_key)
+    tmp = translate(tmp,'fr','en',tr)
     response.add(tmp)
 
-    tmp = translate(text,'en','ja',api_key)
+    tmp = translate(text,'en','ja',tr)
     response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'ja','en',api_key)
+    tmp = translate(tmp,'ja','en',tr)
     response.add(tmp)
 
-    tmp = translate(text,'en','zh',api_key)
+    tmp = translate(text,'en','zh',tr)
     response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'zh','en',api_key)
+    tmp = translate(tmp,'zh','en',tr)
     response.add(tmp)
 
-    tmp = translate(text,'en','de',api_key)
+    tmp = translate(text,'en','de',tr)
     response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'de','en',api_key)
+    tmp = translate(tmp,'de','en',tr)
     response.add(tmp)
     
   if pivot_level == 0 or pivot_level == 2:
-    tmp = translate(text,'en','de',api_key)
+    tmp = translate(text,'en','de',tr)
     response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'de','ru',api_key)
-    tmp = translate(tmp,'ru','en',api_key)
+    tmp = translate(tmp,'de','ru',tr)
+    tmp = translate(tmp,'ru','en',tr)
     response.add(tmp)
 
-    tmp = translate(text,'en','fr',api_key)
-    tmp = translate(tmp,'fr','ru',api_key)
+    tmp = translate(text,'en','fr',tr)
+    tmp = translate(tmp,'fr','ru',tr)
     response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'ru','en',api_key)
+    tmp = translate(tmp,'ru','en',tr)
     response.add(tmp)
 
-    tmp = translate(text,'en','ar',api_key)
-    tmp = translate(tmp,'ar','fr',api_key)
+    tmp = translate(text,'en','ar',tr)
+    tmp = translate(tmp,'ar','fr',tr)
     response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'fr','en',api_key)
+    tmp = translate(tmp,'fr','en',tr)
     response.add(tmp)
 
-    tmp = translate(text,'en','it',api_key)
-    tmp = translate(tmp,'it','ru',api_key)
+    tmp = translate(text,'en','it',tr)
+    tmp = translate(tmp,'it','ru',tr)
     response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'ru','en',api_key)
+    tmp = translate(tmp,'ru','en',tr)
     response.add(tmp)
 
-    tmp = translate(text,'en','ru',api_key)
-    tmp = translate(tmp,'ru','ar',api_key)
+    tmp = translate(text,'en','ru',tr)
+    tmp = translate(tmp,'ru','ar',tr)
     response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'ar','en',api_key)
+    tmp = translate(tmp,'ar','en',tr)
     response.add(tmp)
 
-    tmp = translate(text,'en','ru',api_key)
-    tmp = translate(tmp,'ru','zh',api_key)
+    tmp = translate(text,'en','ru',tr)
+    tmp = translate(tmp,'ru','zh',tr)
     response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'zh','en',api_key)
+    tmp = translate(tmp,'zh','en',tr)
     response.add(tmp)
 
-    tmp = translate(text,'en','ru',api_key)
-    tmp = translate(tmp,'ru','ja',api_key)
+    tmp = translate(text,'en','ru',tr)
+    tmp = translate(tmp,'ru','ja',tr)
     response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'ja','en',api_key)
+    tmp = translate(tmp,'ja','en',tr)
     response.add(tmp)
 
-    tmp = translate(text,'en','ar',api_key)
-    tmp = translate(tmp,'ar','zh',api_key)
+    tmp = translate(text,'en','ar',tr)
+    tmp = translate(tmp,'ar','zh',tr)
     response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'zh','en',api_key)
+    tmp = translate(tmp,'zh','en',tr)
     response.add(tmp)
 
-    tmp = translate(text,'en','ar',api_key)
-    tmp = translate(tmp,'ar','ja',api_key)
+    tmp = translate(text,'en','ar',tr)
+    tmp = translate(tmp,'ar','ja',tr)
     response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'ja','en',api_key)
+    tmp = translate(tmp,'ja','en',tr)
     response.add(tmp)
 
-    tmp = translate(text,'en','de',api_key)
-    tmp = translate(tmp,'de','fr',api_key)
+    tmp = translate(text,'en','de',tr)
+    tmp = translate(tmp,'de','fr',tr)
     response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'fr','en',api_key)
+    tmp = translate(tmp,'fr','en',tr)
     response.add(tmp)
   return response
 
