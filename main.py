886f0878b032cdf74199ed83eb42e4b77240ecae
AudayBerro <audayberro@gmail.com>
2020-07-08 01:18:36 +0200

    add online_translation function

--- main.py
+++ main.py
@@ -34,47 +34,15 @@ def merge_data(dataset1,dataset2):
         v.update(v2) # add dataset2 paraphrases list to dataset1 paraphrases list
     return dataset1
 
-def main():
-    # required arg
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', required=True) # -f data set file name argument
-    parser.add_argument('-g') # if -g is defined use google_translator.translate method not translate_wrapper
-    parser.add_argument('-l') # -l integer that indicate the pivot language level, single-pivot or multi-pivot range between 1 and 3
-    parser.add_argument('-p') # use pretrained translator(p==true - MarianMT) or online translator engine(p==false - Yandex,Google Translator)
-    args = parser.parse_args()
-    
-    # load configs from config.ini file
-    config = configparser.ConfigParser(inline_comment_prefixes="#")
-    config.read(os.path.join(os.path.dirname(__file__), ".","config.ini"))
-    my_memory_config = config["MYMEMORY"]
-    yandex_config = config["YANDEX"]
-    google_config = config["GOOGLE"]
-
-    try:
-        if "email" not in my_memory_config or my_memory_config["email"] == "":
-            raise Exception("Define a Valid email address for MyMemory API in config.ini")
-        else:
-            valid_mail = my_memory_config['email']
-        if "api_key" not in yandex_config or yandex_config["api_key"] == "":
-            raise Exception("Yandex Translate API token is not defined in config.ini")
-        else:
-            yandex_api_key = yandex_config["api_key"]
-        if args.g:#flag g specify to use Official Google Traslator API not a wrapper
-            if "api_key" not in google_config or google_config["api_key"] == "":
-                raise Exception("Google Translate API token is not defined in config.ini")
-            else:
-                google_api_key = google_config['api_key']
-        if args.l:
-            pivot_level = int(args.l)
-        else:
-            pivot_level = 0
-
-    except Exception as e:
-        print(str(e))
-        exit()
-
-    file_path = os.path.join(os.path.dirname(__file__), ".", "dataset/"+args.f) # data to paraphrase
-
+def online_transaltion(file_path,yandex_api_key,valid_mail,pivot_level):
+    """
+    Generate Paraphrases Using online Translator Engine e.g. Google, Yandex
+    :param file_path: file path
+    :param api_key: Yandex Translate API token https://translate.yandex.com/developers/keys
+    :param valid_mail: valid email address to reach a translation rate of 10000 words/day in MyMemory API.
+    :param pivot_level: integer that indicate the pivot language level, single-pivot or multi-pivot range,1 =single-pivot, 2=double-pivot, 0=apply single and double
+    :return a Python dictionary, Key is the initial expression and value is a list of paraphrases
+    """
     #wordnet
     print("Start weak supervision data generation")
     # Generate data by Replacing only word with VERB pos-tags by synonym
@@ -139,6 +107,49 @@ def main():
     print("Start BERT deduplication")
     bert_deduplicate_paraphrases = bert.bert_deduplication(bert_filtered_paraphrases)
     write_to_folder(bert_deduplicate_paraphrases,"BERT deduplication:","paraphrases.txt")
+
+def main():
+    # required arg
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', required=True) # -f data set file name argument
+    parser.add_argument('-g') # if -g is defined use google_translator.translate method not translate_wrapper
+    parser.add_argument('-l') # -l integer that indicate the pivot language level, single-pivot or multi-pivot range between 1 and 3
+    parser.add_argument('-p') # use pretrained translator(p==true - MarianMT) or online translator engine(p==false - Yandex,Google Translator)
+    args = parser.parse_args()
+    
+    # load configs from config.ini file
+    config = configparser.ConfigParser(inline_comment_prefixes="#")
+    config.read(os.path.join(os.path.dirname(__file__), ".","config.ini"))
+    my_memory_config = config["MYMEMORY"]
+    yandex_config = config["YANDEX"]
+    google_config = config["GOOGLE"]
+
+    try:
+        if args.p == "false":
+            if "email" not in my_memory_config or my_memory_config["email"] == "":
+                raise Exception("Define a Valid email address for MyMemory API in config.ini")
+            else:
+                valid_mail = my_memory_config['email']
+            if "api_key" not in yandex_config or yandex_config["api_key"] == "":
+                raise Exception("Yandex Translate API token is not defined in config.ini")
+            else:
+                yandex_api_key = yandex_config["api_key"]
+            if args.g:#flag g specify to use Official Google Traslator API not a wrapper
+                if "api_key" not in google_config or google_config["api_key"] == "":
+                    raise Exception("Google Translate API token is not defined in config.ini")
+                else:
+                    google_api_key = google_config['api_key']
+        if args.l:
+            pivot_level = int(args.l)
+        else:
+            pivot_level = 0
+
+    except Exception as e:
+        print(str(e))
+        exit()
+
+    file_path = os.path.join(os.path.dirname(__file__), ".", "dataset/"+args.f) # data to paraphrase
+    online_transaltion(file_path,yandex_api_key,valid_mail,pivot_level)
 
 if __name__ == "__main__":
     main()
