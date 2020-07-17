7cc7abc9a1a5eff35633db509a2f73c7a565dc0b
AudayBerro <audayberro@gmail.com>
2020-07-17 16:25:41 +0200

    add arccos_similarity function

--- filtering/embedding_service.py
+++ filtering/embedding_service.py
@@ -18,6 +18,21 @@ def get_similarity(vector1,vector2):
     print(cos_lib)
     return np.asscalar(cos_lib)#convert numpy array to float
 
+def arccos_similarity(vector1,vector2):
+    """
+    Return similarity between embeddings vecor. sim(u,v) = 1 - (arcos[(u.v)/(||u||.||v||)] / pi)
+    :param vector1: embedding of the first sentence 
+    :param model: embedding of the second sentence 
+    :return similarity between vecotr1 and vector2
+    """
+    vector1 = vector1.flatten()
+    vector2 = vector2.flatten()
+    dot_uv = np.ndarray.dot(vector1,vector2)
+    norm_uv = np.linalg.norm(vector1) * np.linalg.norm(vector2)
+    arcosine = np.arccos(dot_uv/norm_uv)/np.pi
+    return 1-arcosine
+
+#def filtering(pool,embedding='use', model='use_transformer_large', max_seq_length=128,pooling='reduce_mean'):
 def filtering(pool,embedding='bert', model='bert_base_cased', max_seq_length=128,pooling='reduce_mean'):
     """
     Remove paraphrases that are not semantically equivalent to the initial expression 
@@ -37,7 +52,9 @@ def filtering(pool,embedding='bert', model='bert_base_cased', max_seq_length=128
         for candidate in value:
             vector2 = en.encode([candidate], pooling)
             # b = vector2.reshape(1,-1)
+            # cos_sim = arccos_similarity(vector1,vector2)
             cos_sim = get_similarity(vector1,vector2)
+            print(cos_sim)
             if cos_sim > 0.5:
                 paraphrases.append(candidate)
         result[key] = paraphrases
