fee95ceaf604261e2dd29e7e1985f4fe94c4d5c5
AudayBerro <audayberro@gmail.com>
2020-09-21 22:04:08 +0200

    add google transaltor wrapper

--- translator/deepl_translator.py
+++ translator/deepl_translator.py
@@ -1,4 +1,36 @@
 import requests
+from googletrans import Translator
+
+# def normalize_text(text):
+#     """
+#     Remove line break and lowercase all words
+#     :param text: sentence to normalize
+#     :return return a sentence without line break and lowercased 
+#     """
+#     # mystring.replace('\n', ' ').replace('\r', '')
+#     return text.replace('\n', ' ').replace('\r', '').lower()
+
+def translate_wrapper(sentence,target):
+    """
+    Translate sentence to target language using googletrans library which is a Google Translate API wrapper,
+    call this function when you don't have Google Translate API credentials
+    :param sentence: sentence to translate
+    :param target: target language value can be: ar,fr,en,de,ru,zh,ja,it,... visit for more language https://cloud.com/translate/docs/languages
+    :return translated sentence
+    """
+    translator = Translator()
+    
+    try:
+        # translate the 'text' column
+        response = translator.translate(sentence, dest=target)
+
+    except Exception as e: # mean Google restrict IP address
+        response = "Probably Google has banned your client IP addres"+str(e)
+        return response
+    
+    # return normalize_text(response.text)
+    return response.text
+
 
 def translate(utterance,target,api_key):
     """
@@ -32,68 +64,58 @@ def multi_translate(utterance,api_key,pivot_level):
   text = utterance
   if pivot_level == 0 or pivot_level == 1:
     tmp = translate(text,'IT',api_key)
-    response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'EN',api_key)
-    response.add(tmp)
+    response.add(translate_wrapper(tmp,'en')) # translate back to english with google translator wrapper
+    # tmp = translate(tmp,'EN',api_key) # translate back to english with deepl
+    response.add(translate(tmp,'EN',api_key)) # translate back to english with deepl
 
     tmp = translate(text,'RU',api_key)
-    response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'EN',api_key)
-    response.add(tmp)
+    response.add(translate_wrapper(tmp,'en'))
+    response.add(translate(tmp,'EN',api_key))
 
     tmp = translate(text,'FR',api_key)
-    response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'EN',api_key)
-    response.add(tmp)
+    response.add(translate_wrapper(tmp,'en'))
+    response.add(translate(tmp,'EN',api_key))
 
     tmp = translate(text,'JA',api_key)
-    response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'EN',api_key)
-    response.add(tmp)
+    response.add(translate_wrapper(tmp,'en'))
+    response.add(translate(tmp,'EN',api_key))
 
     tmp = translate(text,'DE',api_key)
-    response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'EN',api_key)
-    response.add(tmp)
+    response.add(translate_wrapper(tmp,'en'))
+    response.add(translate(tmp,'EN',api_key))
     
   if pivot_level == 0 or pivot_level == 2:
     tmp = translate(text,'IT',api_key)
     tmp = translate(tmp,'RU',api_key)
-    response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'EN',api_key)
-    response.add(tmp)
+    response.add(translate_wrapper(tmp,'en'))
+    response.add(translate(tmp,'EN',api_key))
 
     tmp = translate(text,'IT',api_key)
     tmp = translate(tmp,'DE',api_key)
-    response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'EN',api_key)
-    response.add(tmp)
+    response.add(translate_wrapper(tmp,'en'))
+    response.add(translate(tmp,'EN',api_key))
 
     tmp = translate(text,'RU',api_key)
     tmp = translate(tmp,'FR',api_key)
-    response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'EN',api_key)
-    response.add(tmp)
+    response.add(translate_wrapper(tmp,'en'))
+    response.add(translate(tmp,'EN',api_key))
 
     tmp = translate(text,'RU',api_key)
     tmp = translate(tmp,'JA',api_key)
-    response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'EN',api_key)
-    response.add(tmp)
+    response.add(translate_wrapper(tmp,'en'))
+    response.add(translate(tmp,'EN',api_key))
 
     tmp = translate(text,'JA',api_key)
     tmp = translate(tmp,'FR',api_key)
-    response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'EN',api_key)
-    response.add(tmp)
+    response.add(translate_wrapper(tmp,'en'))
+    response.add(translate(tmp,'EN',api_key))
 
     tmp = translate(text,'JA',api_key)
     tmp = translate(tmp,'DE',api_key)
-    response.add(google.translate_wrapper(tmp,'en'))
-    tmp = translate(tmp,'EN',api_key)
-    response.add(tmp)
+    response.add(translate_wrapper(tmp,'en'))
+    response.add(translate(tmp,'EN',api_key))
   return response
 
 if __name__ == "__main__":
-    print(translate('je mange.tu mange."ferme la"','EN','f55c628f-a052-4431-90a7-86d0b8ca861b'))
+    print(multi_translate('How does COVID-19 spread?','f55c628f-a052-4431-90a7-86d0b8ca861b',1))
     
