1d6000cbea926b1505f60489f1df371decb8f4d7
AudayBerro <audayberro@gmail.com>
2020-09-21 20:58:12 +0200

    add multi_translate function

--- translator/deepl_translator.py
+++ translator/deepl_translator.py
@@ -19,6 +19,84 @@ def translate(utterance,target,api_key):
     data = response.json()
     return data['translations'][0]['text']
 
+def multi_translate(utterance,api_key,pivot_level):
+  """
+  Translate sentence
+  :param utterance: sentence to translate
+  :param api_key: Authentication Key for DeepL API https://www.deepl.com/pro-account.html
+  :param pivot_level: integer that indicate the pivot language level, single-pivot or multi-pivot range,1 =single-pivot, 2=double-pivot, 0=apply single and double
+  :return list of utterance translations
+  """
+
+  tr = yandex_translate.Translater() # load yandex translator
+  tr.set_key(api_key) # set the api token
+
+  response = set()
+  text = utterance
+  if pivot_level == 0 or pivot_level == 1:
+    tmp = translate(text,'IT',api_key)
+    response.add(google.translate_wrapper(tmp,'en'))
+    tmp = translate(tmp,'EN',api_key)
+    response.add(tmp)
+
+    tmp = translate(text,'RU',api_key)
+    response.add(google.translate_wrapper(tmp,'en'))
+    tmp = translate(tmp,'EN',api_key)
+    response.add(tmp)
+
+    tmp = translate(text,'FR',api_key)
+    response.add(google.translate_wrapper(tmp,'en'))
+    tmp = translate(tmp,'EN',api_key)
+    response.add(tmp)
+
+    tmp = translate(text,'JA',api_key)
+    response.add(google.translate_wrapper(tmp,'en'))
+    tmp = translate(tmp,'EN',api_key)
+    response.add(tmp)
+
+    tmp = translate(text,'DE',api_key)
+    response.add(google.translate_wrapper(tmp,'en'))
+    tmp = translate(tmp,'EN',api_key)
+    response.add(tmp)
+    
+  if pivot_level == 0 or pivot_level == 2:
+    tmp = translate(text,'IT',api_key)
+    tmp = translate(tmp,'RU',api_key)
+    response.add(google.translate_wrapper(tmp,'en'))
+    tmp = translate(tmp,'EN',api_key)
+    response.add(tmp)
+
+    tmp = translate(text,'IT',api_key)
+    tmp = translate(tmp,'DE',api_key)
+    response.add(google.translate_wrapper(tmp,'en'))
+    tmp = translate(tmp,'EN',api_key)
+    response.add(tmp)
+
+    tmp = translate(text,'RU',api_key)
+    tmp = translate(tmp,'FR',api_key)
+    response.add(google.translate_wrapper(tmp,'en'))
+    tmp = translate(tmp,'EN',api_key)
+    response.add(tmp)
+
+    tmp = translate(text,'RU',api_key)
+    tmp = translate(tmp,'JA',api_key)
+    response.add(google.translate_wrapper(tmp,'en'))
+    tmp = translate(tmp,'EN',api_key)
+    response.add(tmp)
+
+    tmp = translate(text,'JA',api_key)
+    tmp = translate(tmp,'FR',api_key)
+    response.add(google.translate_wrapper(tmp,'en'))
+    tmp = translate(tmp,'EN',api_key)
+    response.add(tmp)
+
+    tmp = translate(text,'JA',api_key)
+    tmp = translate(tmp,'DE',api_key)
+    response.add(google.translate_wrapper(tmp,'en'))
+    tmp = translate(tmp,'EN',api_key)
+    response.add(tmp)
+  return response
+
 if __name__ == "__main__":
     print(translate('je mange.tu mange."ferme la"','EN','f55c628f-a052-4431-90a7-86d0b8ca861b'))
     
