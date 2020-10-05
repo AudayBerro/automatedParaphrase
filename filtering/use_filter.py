0975a73979c8241433173510db0854c4e4eca4f4
AudayBerro <audayberro@gmail.com>
2020-10-05 22:04:51 +0200

    fix session.run() to avoid memory shortage

--- filtering/use_filter.py
+++ filtering/use_filter.py
@@ -17,21 +17,22 @@ def get_embedding(data):
     disable_eager_execution() # to avoid EagerTensor RuntimeError
     embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")
     response = dict()
-    with tf.compat.v1.Session() as session:
-        session.run([tf.compat.v1.global_variables_initializer(), tf.compat.v1.tables_initializer()])
-        for key,value in data.items():
+    for key,value in data.items():
+        with tf.compat.v1.Session() as session:
+            session.run([tf.compat.v1.global_variables_initializer(), tf.compat.v1.tables_initializer()])
             key_embedding = embed([key]) #initial sentence USE embedding
             vector1 = session.run(key_embedding)
+            a=np.reshape(vector1,(1,-1))
             tmp = []
             for candidate in value:
                 candidate_embedding = embed([candidate]) #candidate USE embedding
                 vector2 = session.run(candidate_embedding)
-                
-                a=np.reshape(vector1,(1,-1))
                 b=np.reshape(vector2,(1,-1))
                 cos_lib = cosine_similarity(a,b) #cosine similarity
+                b = 0
                 if cos_lib > 0.5:
                     tmp.append(candidate)
+            session.close()
             response[key]=tmp
 
     return response
