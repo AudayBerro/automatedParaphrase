c24a4994cb53cc4477d4b36ecfccf2f5bd67af89
AudayBerro <audayberro@gmail.com>
2021-03-19 13:33:45 +0100

    REFACTOR add model parameter to generate_paraphrase function

--- Transformers/t5_paraphrases_generation.py
+++ Transformers/t5_paraphrases_generation.py
@@ -54,9 +54,10 @@ def encode_input(tokenizer,text):
     encoding = tokenizer.encode_plus(text,pad_to_max_length=True, return_tensors="pt")
     return encoding
 
-def generate_paraphrase(input_ids,attention_masks,max_len=256):
+def generate_paraphrase(model,input_ids,attention_masks,max_len=256):
     """
     Generate Parpahrases using T5 model
+    :param model: Huggingface T5 model
     :param input_ids: Indices of input sequence tokens in the T5 vocabulary
     :param attention_masks: Mask to avoid performing attention on padding token indices
     :param max_len: The maximum length of the sequence to be generated
@@ -92,48 +93,50 @@ def extract_paraphrases(beam_outputs,tokenizer,utterance):
 def test():
     set_seed(42)
 
-    model = T5ForConditionalGeneration.from_pretrained('./models/model1')#import pre-trained T5 model from ./Transformers/models
-    tokenizer = T5Tokenizer.from_pretrained('t5-base')
+    print("Load T5 model")
+    model = load_model()
+    print("\tsuccess")
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print ("device ",device)
+    print("Load T5 Tokenier")
+    tokenizer = load_tokenizer()
+    print("\tsuccess")
+
+    print("Check GPU availability")
+    device = check_device()
+    print ("\tdevice ",device)
+
+
     model = model.to(device)
 
-    sentence = "how does covid-19 spread"
+    sentence = "how does COVID-19 spread?"
 
+    print("Convert sentence to T5 format ")
+    text =  convert_to_t5_format(sentence)
+    print("\tsuccess")
+    print("\tsentence: ",sentence)
+    print("\tconverted sentence: ",text)
 
-    text =  "paraphrase: " + sentence + " </s>"
+    max_len = 256
 
-    max_len = 256 # The maximum length of the sequence to be generated
+    print("Encode the sentence")
+    encoding = encode_input(tokenizer,text)
+    print("\tsuccess")
 
-    encoding = tokenizer.encode_plus(text,pad_to_max_length=True, return_tensors="pt")
     input_ids, attention_masks = encoding["input_ids"].to(device), encoding["attention_mask"].to(device)
 
 
     # set top_k = 50 and set top_p = 0.95 and num_return_sequences = 3
-    beam_outputs = model.generate(
-        input_ids=input_ids, attention_mask=attention_masks,
-        do_sample=True, #  Whether or not to use sampling ; use greedy decoding otherwise.
-        max_length= max_len, # The maximum length of the sequence to be generated
-        top_k=120, # (int, optional, defaults to 50)– The number of highest probability vocabulary tokens to keep for top-k-filtering.
-        top_p=0.98, # (float, optional, defaults to 1.0)– If set to float < 1, only the most probable tokens with probabilities that add up to top_p or higher are kept for generation.
-        early_stopping=True, # Whether to stop the beam search when at least num_beams sentences are finished per batch or not.
-        num_return_sequences=10 # (int, optional, defaults to 1) – The number of independently computed returned sequences for each element in the batch.
-    )
+    print("Generated Parpahrases")
+    beam_outputs = generate_paraphrase(model,input_ids,attention_masks,max_len)
+    print("\tsuccess")
 
-
-    print ("\nOriginal Question ::")
+    print (" Original Question ::")
     print (sentence)
-    print ("\n")
-    print ("Paraphrased Questions :: ")
-    final_outputs =[]
-    for beam_output in beam_outputs:
-        sent = tokenizer.decode(beam_output, skip_special_tokens=True,clean_up_tokenization_spaces=True)
-        if sent.lower() != sentence.lower() and sent not in final_outputs:
-            final_outputs.append(sent)
+    print ("\nParaphrased Questions :: ")
+    final_outputs = extract_paraphrases(beam_outputs,tokenizer,sentence)
 
     for i, final_output in enumerate(final_outputs):
-        print("{}: {}".format(i, final_output))
+        print("\t{}: {}".format(i, final_output))
 
 if __name__ == "__main__":
     test()
