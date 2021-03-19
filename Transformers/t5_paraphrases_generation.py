90fa75fb37cc3f2cc485e2681cb6b36d04e45f27
AudayBerro <audayberro@gmail.com>
2021-03-19 13:11:00 +0100

    add generate_paraphrase function

--- Transformers/t5_paraphrases_generation.py
+++ Transformers/t5_paraphrases_generation.py
@@ -53,6 +53,27 @@ def encode_input(tokenizer,text):
     encoding = tokenizer.encode_plus(text,pad_to_max_length=True, return_tensors="pt")
     return encoding
 
+def generate_paraphrase(input_ids,attention_masks,max_len=256):
+    """
+    Generate Parpahrases using T5 model
+    :param input_ids: Indices of input sequence tokens in the T5 vocabulary
+    :param attention_masks: Mask to avoid performing attention on padding token indices
+    :param max_len: The maximum length of the sequence to be generated
+    return list of generated paraphrases in a form of torch.FloatTensor or dictionary if config.return_dict=True(in this code is set to False)
+    """
+    # set top_k = 50 and set top_p = 0.95 and num_return_sequences = 3
+    beam_outputs = model.generate(
+        input_ids=input_ids, attention_mask=attention_masks,
+        do_sample=True, #  Whether or not to use sampling ; use greedy decoding otherwise.
+        max_length= max_len, # The maximum length of the sequence to be generated
+        top_k=120, # (int, optional, defaults to 50)– The number of highest probability vocabulary tokens to keep for top-k-filtering.
+        top_p=0.98, # (float, optional, defaults to 1.0)– If set to float < 1, only the most probable tokens with probabilities that add up to top_p or higher are kept for generation.
+        early_stopping=True, # Whether to stop the beam search when at least num_beams sentences are finished per batch or not.
+        num_return_sequences=10 # (int, optional, defaults to 1) – The number of independently computed returned sequences for each element in the batch.
+    )
+    return beam_outputs
+
+
 def test():
     set_seed(42)
 
@@ -68,7 +89,7 @@ def test():
 
     text =  "paraphrase: " + sentence + " </s>"
 
-    max_len = 256
+    max_len = 256 # The maximum length of the sequence to be generated
 
     encoding = tokenizer.encode_plus(text,pad_to_max_length=True, return_tensors="pt")
     input_ids, attention_masks = encoding["input_ids"].to(device), encoding["attention_mask"].to(device)
@@ -78,7 +99,7 @@ def test():
     beam_outputs = model.generate(
         input_ids=input_ids, attention_mask=attention_masks,
         do_sample=True, #  Whether or not to use sampling ; use greedy decoding otherwise.
-        max_length=256, # The maximum length of the sequence to be generated
+        max_length= max_len, # The maximum length of the sequence to be generated
         top_k=120, # (int, optional, defaults to 50)– The number of highest probability vocabulary tokens to keep for top-k-filtering.
         top_p=0.98, # (float, optional, defaults to 1.0)– If set to float < 1, only the most probable tokens with probabilities that add up to top_p or higher are kept for generation.
         early_stopping=True, # Whether to stop the beam search when at least num_beams sentences are finished per batch or not.
