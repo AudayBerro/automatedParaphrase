3f78e62798e495f9ed47caaef9bfe57057d2e156
AudayBerro <audayberro@gmail.com>
2021-05-06 22:21:12 +0200

    submit form through AJAX

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,11 @@
-function displayResult(){
-    setTimeout(addParaphrases,3000);
-}
-
-function addParaphrases(){
-    var a = ['will contain generated paraphrases']
-        document.getElementById('box').innerHTML=''
-    for(var i=0;i<a.length;i++ ){
-        var element = document.createElement('li');
-        element.innerHTML=a[i]
-        document.getElementById('box').appendChild(element)
-    }
-}
+var serializeForm = function (form) {
+	var obj = {};
+	var formData = new FormData(form);
+	for (var key of formData.keys()) {
+		obj[key] = formData.get(key);
+	}
+	return obj;
+};
 
 document.getElementById('monselect').addEventListener('change',function(){
     var disable = false;
@@ -22,4 +17,36 @@ document.getElementById('monselect').addEventListener('change',function(){
     e.disabled = disable
     })
     
+})
+
+document.addEventListener('submit',function(event){
+    event.preventDefault();
+// console.log(new FormData(document.querySelector('#mainform')));//return false;
+    document.querySelector('.result').classList.add('loading') ;
+	fetch('http://localhost:5000/', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        },
+		body: JSON.stringify(serializeForm(event.target)),
+	}).then(function (response) {
+		if (response.ok) {
+			return response.json();
+		}
+		return Promise.reject(response);
+	}).then(function (data) {
+
+        document.querySelector('.result').classList.remove('loading') ;
+        document.getElementById('box').innerHTML='';
+            for(var key in data){
+                for(var elem in data[key]){
+                    var element = document.createElement('li');
+                    element.innerHTML = data[key][elem]
+                    document.getElementById('box').appendChild(element)
+                }
+            }
+        
+	}).catch(function (error) {
+		console.warn(error);
+	});
 })
