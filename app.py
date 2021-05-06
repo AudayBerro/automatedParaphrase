ed2030ff477dbbd121990e092546dbe600698495
AudayBerro <audayberro@gmail.com>
2021-05-06 22:52:32 +0200

    disable radio if Pivot not present in configuration

--- app.py
+++ app.py
@@ -16,14 +16,17 @@ def index():
         # get user sentence
         sentence = data['user_utterance']
 
-        # check if pivot-level radio option is not disabled
-        if data['pivot_level']:
-            # get selected pivot level option: 1-pivot or 2-pivot
-            pivot_level = data['pivot_level']#return string
+        #check if User configuration contain Pivot-transaltion component: config != ['c2','c3','c5','c12']
+        non_pivot_config = ['c2','c3','c5','c12']
+        if config not in non_pivot_config:
+            # check if pivot-level radio option is not disabled
+            if data['pivot_level']:
+                # get selected pivot level option: 1-pivot or 2-pivot
+                pivot_level = data['pivot_level']#return string
 
-            # Machine Translator option: pre-trained MT(e.g. Huggingface Marian MT) or Online MT model(Deepl,Google)
-            pre_trained_mt = data['pre_trained_mt']#return string
-            paraphrases = main.generate_from_gui(sentence,config,pivot_level,pre_trained_mt)
+                # Machine Translator option: pre-trained MT(e.g. Huggingface Marian MT) or Online MT model(Deepl,Google)
+                pre_trained_mt = data['pre_trained_mt']#return string
+                paraphrases = main.generate_from_gui(sentence,config,pivot_level,pre_trained_mt)
         else:
             paraphrases = main.generate_from_gui(sentence,config)
         return jsonify(paraphrases)
