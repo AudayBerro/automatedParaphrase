d6bc3764b5d3c568dfad0885f66493a2c7582c3c
AudayBerro <audayberro@gmail.com>
2021-05-17 18:30:22 +0200

    add test() function

--- translator/open_nmt.py
+++ translator/open_nmt.py
@@ -27,6 +27,25 @@ def load_model(model):
     else:
         return None
 
+
+def test():
+    #Load Open-NMT translation model
+    print("\nLoad Open-NMT translation model:")
+    print("\tload English to German model")
+    en2de = load_model("en-de")
+
+    print("\tload German to English model")
+    de2en = load_model("de-en")
+
+    #strat translation
+    tmp = "sentence to translate to german"
+    tmp2 = translate(tmp,en2de)
+
+    #translate back to english
+    tmp3 = translate(tmp2,de2en)
+
+    print("German: ",tmp2," - ENglish: ",tmp3)
+
 if __name__ == "__main__":
     # Round-trip translations between English and German:
     en2de = torch.hub.load('pytorch/fairseq', 'transformer.wmt19.en-de.single_model', tokenizer='moses', bpe='fastbpe')
